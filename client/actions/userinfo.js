4f1353a7466761e10b74aedf0be0e938b2e0db50
asddsa356 <asddsa356@mail.ru>
2018-07-19 18:44:28 +0300

    add userinfo

--- client/actions/userinfo.js
+++ client/actions/userinfo.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
-import { FETCH_USERINFO_START, FETCH_USERINFO_SUCCESS, FETCH_USERINFO_FAILED } from '../constant/userinfo-consts';
+import {
+  FETCH_USERINFO_START,
+  FETCH_USERINFO_SUCCESS,
+  FETCH_USERINFO_FAILED,
+  UPDATE_USERINFO_START,
+  UPDATE_USERINFO_SUCCESS,
+  UPDATE_USERINFO_FAILED
+} from '../constant/userinfo-consts';
 const host = require('../config').host;
 export function fetchUserInfo() {
   return (dispatch, getState) => {
@@ -15,12 +22,75 @@ export function fetchUserInfo() {
         }
       })
       .then(res => {
-        console.log(res.data.self);
         dispatch({
           type: FETCH_USERINFO_SUCCESS,
           payload: res.data.self
         });
       })
       .catch(err => dispatch({ type: FETCH_USERINFO_FAILED }));
+  };
+}
+
+export function updateUserInfo(state) {
+  return (dispatch, getState) => {
+    console.log(state);
+    const userid = getState().userinfo.userinfo.id;
+    const infoid = getState().userinfo.userinfo.info.id;
+    const hash = localStorage.getItem('hash');
+    dispatch({ type: UPDATE_USERINFO_START });
+    try {
+      axios
+        .patch(
+          `${host}/users/${userid}/`,
+          JSON.stringify({
+            name: state.name,
+            family_name: state.family_name,
+            email: state.email
+          }),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Basic ' + hash
+            }
+          }
+        )
+
+        .then(res => {
+          console.log(res);
+
+          dispatch({
+            type: UPDATE_USERINFO_SUCCESS
+          });
+        });
+
+      axios
+        .patch(
+          `${host}/userinfo/${infoid}/`,
+          JSON.stringify({
+            eth_account: state.eth_account,
+            organization_name: state.organization_name,
+            tax_num: state.tax_num,
+            payment_num: state.payment_num,
+            files: '',
+            user: userid
+          }),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Basic ' + hash
+            }
+          }
+        )
+
+        .then(res => {
+          console.log(res);
+
+          dispatch({
+            type: UPDATE_USERINFO_SUCCESS
+          });
+        });
+    } catch (err) {
+      dispatch({ type: UPDATE_USERINFO_FAILED });
+    }
   };
 }
--- client/reducers/userinfo-reducer.js
+++ client/reducers/userinfo-reducer.js
@@ -1,8 +1,14 @@
-import { FETCH_USERINFO_START, FETCH_USERINFO_SUCCESS, FETCH_USERINFO_FAILED } from '../constant/userinfo-consts';
-
+import {
+  FETCH_USERINFO_START,
+  FETCH_USERINFO_SUCCESS,
+  FETCH_USERINFO_FAILED,
+  UPDATE_USERINFO_START,
+  UPDATE_USERINFO_SUCCESS,
+  UPDATE_USERINFO_FAILED
+} from '../constant/userinfo-consts';
 const initialState = {
   preloader: false,
-  userinfo: {}
+  userinfo: { info: {} }
 };
 export default function userInfoReducer(state = initialState, action) {
   switch (action.type) {
@@ -15,6 +21,12 @@ export default function userInfoReducer(state = initialState, action) {
         userinfo: action.payload
       };
     case FETCH_USERINFO_FAILED:
+      return { ...state, preloader: false };
+    case UPDATE_USERINFO_START:
+      return { ...state, preloader: true };
+    case UPDATE_USERINFO_SUCCESS:
+      return { ...state, preloader: false };
+    case UPDATE_USERINFO_FAILED:
       return { ...state, preloader: false };
   }
 
