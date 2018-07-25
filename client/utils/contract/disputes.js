24fd3dd4da69b6ec0cece6d20c70b51ca2d98319
Anatoly Bardukov <sindbag@gmail.com>
2018-07-25 04:52:50 +0300

    Update abi

--- client/utils/contract/disputes.js
+++ client/utils/contract/disputes.js
@@ -3,6 +3,6 @@ import web3 from './web3';
 
 const contract = {
   address: '0x627e5649c7d3fdb24768b494a65a781ced83dcac',
-  abi: abi
+  abi
 };
 export default new web3.eth.Contract(contract.abi, contract.address);
